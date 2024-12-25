96f182383ebd1ccd7b7ea86a6fb515bdf9b2d1a2
naoyatakayama <kanskce7541@gmail.com>
2024-12-25 23:59:54 +0900

    solve problem: 876

---
+++ Easy/876. Middle of the Linked List/solution.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        num = 1
+        head2 = head
+        while head.next != None:
+            num += 1
+            head = head.next
+        
+        for i in range(0, num//2):
+            head2 = head2.next
+        
+        return head2
+
+# another solution
+class Solution(object):
+    def middleNode(self, head):
+        # While slow moves one step forward, fast moves two steps forward.
+        # Finally, when fast reaches the end, slow happens to be in the middle of the linked list.
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
